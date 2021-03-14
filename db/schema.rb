78a052267f765d8bd8097cd0527995df353ffc3d
Rehan <rehanmuh@grinnell.edu>
2021-03-13 21:30:35 -0600

    User model added with validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_022806) do
+ActiveRecord::Schema.define(version: 2021_03_14_025910) do
 
   create_table "quotes", force: :cascade do |t|
     t.string "desc"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_02_18_022806) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
